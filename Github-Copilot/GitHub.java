10de82806ad5028ffc03d7de12129d847952e1a5
Sathesh Kumar Murugan <satheshkumar.m@ad.infosys.com>
2023-03-30 10:10:40 +0530

    added github copilot sample testcode

---
+++ Github-Copilot/GitHub.java
@@ -0,0 +1,13 @@
+public class GitHub {
+    public static void main(String[] args) {
+        // write a code for reverse String
+        String str = "Hello World";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        System.out.println(rev);
+
+    
+    }
+}
---
+++ Github-Copilot/Github.js
@@ -0,0 +1,7 @@
+// write a code for reverse String and print it output
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString("hello"));
